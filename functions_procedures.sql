d144b65eb7960801d085c4aaeec23cac8498c6dc
221049 Kusiak Juliusz <221049@edu.p.lodz.pl>
2020-12-20 19:00:21 +0100

    Added new package containing API operating on table tracking data about daily visits, added such table

--- functions_procedures.sql
+++ functions_procedures.sql
@@ -1,43 +1,74 @@
 SET SERVEROUTPUT ON;
 
--- 6 procedures
+CREATE TABLE daily_visits (
+    id NUMBER NOT NULL,
+    day_measured DATE NOT NULL,
+    visitors_number NUMBER NOT NULL,
+    mean_visit_time NUMBER NOT NULL
+);
+
+
+CREATE SEQUENCE id_seq
+    INCREMENT BY 1
+    START WITH 1;
 
 
 
 
--- 4 functions
--- 1. MEAN_TIME_VISITING_FUN(NUMBER day, NUMBER month, NUMBER year) returns mean visiting time for visitors in a given day
 
-
-
-
-create or replace FUNCTION MEAN_TIME_VISITING_FUN(i_entry_day IN NUMBER, i_entry_month IN NUMBER, i_entry_year IN NUMBER)
-RETURN NUMBER
+CREATE OR REPLACE PACKAGE DAILY_VISITS_TRACKER_API
 IS
-    l_total_daily_time NUMBER := 0;
-    l_mean_daily NUMBER := 0;
-    tot_rows NUMBER := 0;
+    CURSOR c_visitors RETURN visitors%ROWTYPE;
+    FUNCTION TIME_STAYING_FUNC(visitor c_visitors%ROWTYPE) RETURN NUMBER;
+    PROCEDURE DAILY_VISITS_UPDATE_PRC(l_mean_daily OUT NUMBER);
+END DAILY_VISITS_TRACKER_API;
 
-    CURSOR c_visitors 
+
+
+
+CREATE OR REPLACE PACKAGE BODY DAILY_VISITS_TRACKER_API
+IS
+    g_current_time TIMESTAMP := sysdate;
+   
+    CURSOR c_visitors RETURN visitors%ROWTYPE
     IS
         SELECT *
         FROM visitors
-        WHERE EXTRACT(DAY FROM time_of_entrance) = i_entry_day
-        AND EXTRACT(MONTH FROM time_of_entrance) = i_entry_month
-        AND EXTRACT(YEAR FROM time_of_entrance) = i_entry_year;
+        WHERE EXTRACT(DAY FROM time_of_entrance) = EXTRACT(DAY FROM g_current_time)
+        AND EXTRACT(MONTH FROM time_of_entrance) = EXTRACT(MONTH FROM g_current_time)
+        AND EXTRACT(YEAR FROM time_of_entrance) = EXTRACT(YEAR FROM g_current_time);
+        
+        
+        
+    FUNCTION TIME_STAYING_FUNC(visitor c_visitors%ROWTYPE) 
+    RETURN NUMBER
+    IS
+        l_visitor_daily_time NUMBER := 0;
 
-BEGIN
-    FOR visitor IN c_visitors
-    LOOP
-        l_total_daily_time := l_total_daily_time
-                            + TO_NUMBER(EXTRACT(HOUR FROM visitor.time_of_leaving - visitor.time_of_entrance)) * 60
-                            + TO_NUMBER(EXTRACT(MINUTE FROM visitor.time_of_leaving - visitor.time_of_entrance));
-        tot_rows := tot_rows + 1;
-    END LOOP;
+    BEGIN
+        l_visitor_daily_time := TO_NUMBER(EXTRACT(HOUR FROM visitor.time_of_leaving - visitor.time_of_entrance)) * 60
+                                + TO_NUMBER(EXTRACT(MINUTE FROM visitor.time_of_leaving - visitor.time_of_entrance));
+        RETURN l_visitor_daily_time;
+    END;
 
-    l_mean_daily := ROUND((l_total_daily_time / tot_rows), 0);
-    
-    
-    RETURN l_mean_daily;
-END;
+
+
+    PROCEDURE DAILY_VISITS_UPDATE_PRC(l_mean_daily OUT NUMBER)
+    IS
+        l_total_daily_time NUMBER := 0;
+        tot_visitors NUMBER := 0;
+        
+    BEGIN
+        FOR visitor IN c_visitors
+        LOOP
+            l_total_daily_time := l_total_daily_time + TIME_STAYING_FUNC(visitor);
+            tot_visitors := tot_visitors + 1;
+        END LOOP;
+
+        l_mean_daily := ROUND((l_total_daily_time / tot_visitors), 0);
+        
+        INSERT INTO daily_visits(id, day_measured, visitors_number, mean_visit_time)
+        VALUES(id_seq.NEXTVAL, g_current_time, tot_visitors, l_mean_daily);
+    END;
+END DAILY_VISITS_TRACKER_API;
 
